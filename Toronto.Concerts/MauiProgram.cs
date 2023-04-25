fa830827ca2701e04916014d26789b193e970f42
jamiefraser <jamiefraser@teksavvy.com>
2023-04-25 10:54:10 -0400

    Trying to get production deployment working

--- Toronto.Concerts/MauiProgram.cs
+++ Toronto.Concerts/MauiProgram.cs
@@ -3,6 +3,7 @@ using Microsoft.Fast.Components.FluentUI;
 using Microsoft.Maui.Controls.PlatformConfiguration;
 using Toronto.Concerts.Data;
 using Toronto.Concerts.Services;
+using MauiInsights;
 
 namespace Toronto.Concerts;
 
@@ -13,30 +14,26 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .AddApplicationInsights("InstrumentationKey=a2fc8a11-314b-42a4-9255-13f7e62cdac8;IngestionEndpoint=https://canadacentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://canadacentral.livediagnostics.monitor.azure.com/")
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
             });
-
         builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddFluentUIComponents();
 
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
 #endif
 #if IOS
-        if (DeviceInfo.Current.Platform == DevicePlatform.iOS)
-        {
-            builder.Services.AddSingleton<ICalendarService, Toronto.Concerts.Platforms.iOS.CalendarService>();
-        }
+        builder.Services.AddSingleton<ICalendarService, Toronto.Concerts.Platforms.iOS.CalendarService>();
 #endif
-
 #if ANDROID
-    builder.Services.AddSingleton<ICalendarService, Toronto.Concerts.Platforms.Android.CalendarService>();
+        //builder.Services.AddSingleton<ICalendarService, Toronto.Concerts.Platforms.Android.CalendarService>();
 #endif
-        builder.Services.AddSingleton<WeatherForecastService>();
+        //builder.Services.AddSingleton<WeatherForecastService>();
         builder.Services.AddSingleton<IConcertDataService, ConcertDataService>();
-        builder.Services.AddFluentUIComponents();
 
         return builder.Build();
     }
