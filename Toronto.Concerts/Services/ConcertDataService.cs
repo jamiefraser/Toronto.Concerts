81b3dc5d5148f8d5ad2050b210190f9c2841247b
Jamie Fraser <Peter.Fraser@mbg.cognizant.com>
2023-04-26 17:43:29 -0400

    Added caching

--- Toronto.Concerts/Services/ConcertDataService.cs
+++ Toronto.Concerts/Services/ConcertDataService.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using MonkeyCache.FileStore;
+using System.ComponentModel;
+using System.Net.Http.Json;
 using System.Text;
 using Toronto.Concerts.Data;
 
@@ -7,12 +9,12 @@ namespace Toronto.Concerts.Services
     public class ConcertDataService : IConcertDataService
     {
         HttpClient _client;
-        private List<Concert> concerts;
+        private List<Concert> concerts = new List<Concert>();
         public ConcertDataService()
         {
             _client = new HttpClient();
         }
-        private bool busy;
+        private bool busy = true;
 
         public bool Busy
         {
@@ -65,53 +67,62 @@ namespace Toronto.Concerts.Services
                 }
             }
         }
-        public async Task<bool> GetConcerts()
+        async Task<IEnumerable<Concert>> GetConcertsAsync()
         {
-            var ret = false;
-            Busy = true;
-            try
+            var url = "https://www.thewholenote.com/ludwig/listings/search.php";
+            Barrel.ApplicationId = "Toronto.Concerts";
+            //Dev handle online/offline scenario
+            //if (!CrossConnectivity.Current.IsConnected)
+            //{
+            //    return Barrel.Current.Get<IEnumerable<Concert>>(key: url);
+            //}
+
+            //Dev handles checking if cache is expired
+            if (!Barrel.Current.IsExpired(key: url))
             {
-                var location = new Data.Location() { id = "1", name = "City of Toronto" };
-                var today = DateTime.Now.Date.ToUniversalTime();
-                var concertQuery = new ConcertQuery()
-                {
-                    startDate = (int)DateTimeOffset.Now.ToUnixTimeSeconds(),
-                    endDate = (int)DateTimeOffset.Now.AddDays(30).ToUnixTimeSeconds(),
-                    tags = new List<string>().ToArray(),
-                    enhancedOnly = false,
-                    locations = new Data.Location[] { location, new Data.Location() { id = "2", name = "Halton-Peel Regions" }, new Data.Location() { id = "3", name = "York Region" }, new Data.Location() { id = "4", name = "Durham Region" } }
-                };
-                System.Diagnostics.Debug.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(concertQuery));
-                //{"query":"","listingId":null,"enhancedOnly":false,"startDate":1681358400,"endDate":1683432000,"locations":[{"id":"1","name":"City+of+Toronto"},{"id":"2","name":"Halton-Peel+Regions"},{"id":"3","name":"York+Region"},{"id":"4","name":"Durham+Region"}],"tags":["Chamber","Choral","Early/Baroque","Musical+Theatre","New+Music","Organ","Orchestra","Piano","Solo+Voice","Strings","Religious+Service"]}
-                var formContent = new FormUrlEncodedContent(new[]
-                                                            {
+                Concerts = Barrel.Current.Get<IEnumerable<Concert>>(key: url).ToList();
+                return Concerts;
+            }
+
+            var location = new Data.Location() { id = "1", name = "City of Toronto" };
+            var today = DateTime.Now.Date.ToUniversalTime();
+            var concertQuery = new ConcertQuery()
+            {
+                startDate = (int)DateTimeOffset.Now.ToUnixTimeSeconds(),
+                endDate = (int)DateTimeOffset.Now.AddDays(30).ToUnixTimeSeconds(),
+                tags = new List<string>().ToArray(),
+                enhancedOnly = false,
+                locations = new Data.Location[] { location, new Data.Location() { id = "2", name = "Halton-Peel Regions" }, new Data.Location() { id = "3", name = "York Region" }, new Data.Location() { id = "4", name = "Durham Region" } }
+            };
+            System.Diagnostics.Debug.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(concertQuery));
+            //{"query":"","listingId":null,"enhancedOnly":false,"startDate":1681358400,"endDate":1683432000,"locations":[{"id":"1","name":"City+of+Toronto"},{"id":"2","name":"Halton-Peel+Regions"},{"id":"3","name":"York+Region"},{"id":"4","name":"Durham+Region"}],"tags":["Chamber","Choral","Early/Baroque","Musical+Theatre","New+Music","Organ","Orchestra","Piano","Solo+Voice","Strings","Religious+Service"]}
+            var formContent = new FormUrlEncodedContent(new[]
+                                                        {
                                                                 new KeyValuePair<string, string>("query", Newtonsoft.Json.JsonConvert.SerializeObject(concertQuery))
                                                             });
 
-                var concertsResponse = await _client.PostAsync("https://www.thewholenote.com/ludwig/listings/search.php", formContent);
-                var json = await concertsResponse.Content.ReadAsStringAsync();
-                System.Diagnostics.Debug.WriteLine(json);
-                Concerts = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Concert>>(json);
-                var todaysConcerts = Concerts.Where(concert => concert.DateAndTime.Date.Equals(DateTime.Now.Date)).ToList();
-                if (DeviceInfo.Current.Platform.Equals(DevicePlatform.WinUI))
-                {
-                    System.IO.Directory.SetCurrentDirectory("c:\\git\\toronto.concerts");
-                    using (var s = System.IO.File.OpenWrite("concerts.json"))
-                    {
+            var concertsResponse = await _client.PostAsync("https://www.thewholenote.com/ludwig/listings/search.php", formContent);
+            var json = await concertsResponse.Content.ReadAsStringAsync();
+            System.Diagnostics.Debug.WriteLine(json);
+            Concerts = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Concert>>(json);
 
-                        await (s.WriteAsync(Encoding.ASCII.GetBytes(json)));
-                    }
-                    var addresses = Concerts.Select(c => c.address);
-                    var uniqueAddresses = addresses.Distinct();
-                    using (var sa = System.IO.File.OpenWrite("addresses.json"))
-                    {
-                        await (sa.WriteAsync(Encoding.ASCII.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(uniqueAddresses))));
-                    }
-                }
+            //Saves the cache and pass it a timespan for expiration
+            Barrel.Current.Add(key: url, data: concerts, expireIn: TimeSpan.FromDays(1));
+            return concerts;
+        }
+        public async Task<bool> GetConcerts()
+        {
+            Busy = true;
+            var ret = false;
+            try
+            {
+                await GetConcertsAsync();
+                var todaysConcerts = Concerts.Where(concert => concert.DateAndTime.Date.Equals(DateTime.Now.Date)).ToList();
                 ret = true;
             }
             catch (Exception ex)
             {
+                System.Diagnostics.Debug.WriteLine(ex.Message + "\r\n" + ex.StackTrace);
             }
             finally
             {
